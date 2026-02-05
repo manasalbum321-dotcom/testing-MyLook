21f3eb970acdecadb02c62b341fcf3f8d0743c71
MANAS PRAJAPATI <manasalbum321@gmail.com>
2026-02-06 00:37:07 +0530

    Change Firebase config for new project setup
    Updated Firebase configuration with new project details.

--- signup.js
+++ signup.js
@@ -5,10 +5,13 @@ import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/12.7.0
 
 // 🔥 Firebase config (APNA CONFIG YAHA PASTE KARO)
 const firebaseConfig = {
-  apiKey: "AIzaSyDPlU4bi3FUyv16Hm2ZKn9QaYXN__n4u5E",
-    authDomain: "mylook-25be4.firebaseapp.com",
-    databaseURL: "https://mylook-25be4-default-rtdb.firebaseio.com",
-    projectId: "mylook-25be4",
+  apiKey: "AIzaSyBkH7gqLLLtvQfH8buj4LJmlIN9ypu4_Hc",
+    authDomain: "mylook-testing.firebaseapp.com",
+    projectId: "mylook-testing",
+    storageBucket: "mylook-testing.firebasestorage.app",
+    messagingSenderId: "326729149691",
+    appId: "1:326729149691:web:bbf601f07589912734d45e",
+    measurementId: "G-QGME0V3Q8S"
 };
 
 // Initialize Firebase
